e045eb01e7c18974925614e68eb5c66f915d5315
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-04-08 12:37:26 +1000

    finsihed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+
+function superbowlWin(record){
+    // if record.find returns a value = good else it returns undefined
+    let res = record.find(ending => ending.result === "W") 
+    if(res)
+    {return res.year} 
+    else return res
+  
+}
+
+
+superbowlWin(record)
+
+
